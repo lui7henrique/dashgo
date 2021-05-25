97d70047b0f23bda165fe44dbf722b981e7a8dc2
Luiz Henrique <7henrique18@gmail.com>
2021-05-25 15:24:43 -0300

    :jigsaw: Login e dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,93 @@
+import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
+import dyanamic from 'next/dynamic'
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+const Chart = dyanamic(() => import('react-apexcharts'), {
+  ssr: false,
+}) 
+
+const options ={
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  }, 
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-11-18T00:00:00:00.000Z',
+      '2021-11-19T00:00:00:00.000Z',
+      '2021-11-20T00:00:00:00.000Z',
+      '2021-11-21T00:00:00:00.000Z',
+      '2021-11-22T00:00:00:00.000Z',
+      '2021-11-23T00:00:00:00.000Z',
+      '2021-11-24T00:00:00:00.000Z'
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
+  }
+}
+
+const series = [
+  { name: 'series1', data: [18, 30, 28, 48, 19, 70, 50]}
+]
+
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" algin="flex-start">
+         <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4"> Inscritos da semana</Text>
+            <Chart options={options} series={series} type="area" eight={160}></Chart>
+          </Box>
+         <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            pb="4"
+          >
+            <Text fontSize="lg" mb="4"> Taxa de abertura</Text>
+            <Chart options={options} series={series} type="area" eight={160}></Chart>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,28 @@
-import Head from "next/head";
-import Image from "next/image";
-
-export default function Home() {
-  return <h1>dashgo</h1>;
+import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+export default function SignIn() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      justify="center"
+      align="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email"/>
+          <Input name="password" label="Senha" type="password"/>                  
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
+  );
 }
